19aa3b8c9b07243ff6352c11aabdc62cf3399238
Victor Wangechi <murkyvictor@gmail.com>
2022-06-14 20:45:06 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate") {
+    //function created
+    return `This Saturday, I want to ${activity}!`
+  }
+  
+  const mondayWork = function
+  (activity="go to the office") {
+
+    return `This Monday, I will ${activity}.`
+  }
+  //defined mondayWork functione xpression
+
+  const wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+
+//function that wraps an adjective.
+
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
